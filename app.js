cacaa09f0f0763c92cf5065ade935329bec4b3d4
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-08-03 12:59:58 -0700

    app.js now automatically builds grid boxes on index.html

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
 // tetris game app
 
-class Tetromino {
+//----- constants
+const tetrisCotainer = document.querySelector('[data-tetrisContainer]');
+
+
+
+
+class Tetris {
+
+    // buildGame
+    buildGame() {
+        // total number of grids allowed in the grid container
+        let totalNumberOfGridBoxes = 200;
+        // count how many grid boxes have been built
+        let numberOfBoxesBuilt = 0;
+
+
+
+        while (numberOfBoxesBuilt < totalNumberOfGridBoxes) {
+            // create box
+            let createBox = document.createElement('div');
+            let appendBox = tetrisCotainer.appendChild(createBox);
+            numberOfBoxesBuilt++;
+            console.log('yes');
+        }
+        console.log(tetrisCotainer);
+    }
+
+    // startGame;
+    startGame() {}
+
+    // pause game
+    pauseGame() {}
+
+    // stop / complete / end game
+    endGame() {}
 
     // randomly select tetromino
-    selectTetrominoes() {}
+    selectTetromino() {}
 
     // ----move tetromino
 
+    // rotate Tetramino
+    rotateTetramino() {}
+
     // move tetromino left
-    moveLeft() {}
+    moveTetrominoLeft() {}
 
     //  tetromino right
-    moveRight() {}
+    moveTetraminoRight() {}
 
     //move tetromino down
-    moveDown() {}
+    moveTetraminoDown() {}
 
     // pull tetromino down
-    pullDown() {}
-
+    pullTetraminoDown() {}
 
 }
+
+const tetris = new Tetris();
+
+
+// add grid boxes to display tetraminoes inside the tetris container
+tetris.buildGame();
