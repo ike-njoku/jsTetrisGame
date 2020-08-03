c128357676dc743fb5e07bd13ac79933ba8af993
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-08-03 01:43:08 -0700

    added boiler plate to files, added background colour to index file

---
+++ app.js
@@ -0,0 +1,23 @@
+// tetris game app
+
+class Tetromino {
+
+    // randomly select tetromino
+    selectTetrominoes() {}
+
+    // ----move tetromino
+
+    // move tetromino left
+    moveLeft() {}
+
+    //  tetromino right
+    moveRight() {}
+
+    //move tetromino down
+    moveDown() {}
+
+    // pull tetromino down
+    pullDown() {}
+
+
+}
