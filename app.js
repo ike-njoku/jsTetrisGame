7e759dd1b289971abd4b26e3bb5d0cd90a0c580f
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-08-05 01:32:39 -0700

    restructured css and app.js --- updates to tetris.nuildGameEnvironment

--- app.js
+++ app.js
@@ -1,65 +1,99 @@
-// tetris game app
+// tetris game
 
-//----- constants
-const tetrisCotainer = document.querySelector('[data-tetrisContainer]');
+//------------- define tetraminoes
+// there are five tetraminoes and each have been rotated
+// therefore each tetramino has an array of possible rotations
+// so theres an two-dimensional array
 
+const tetraminoes = [
+    ['T-shape', [1, 2, 3, 5],
+        [2, 4, 5, 6],
+        [1, 4, 5, 7]
+    ]
+];
+
+// the start position for each tetramino
+const tetraminoStartPosition = [0, 5];
 
 
 
 class Tetris {
-
-    // buildGame
-    buildGame() {
-        // total number of grids allowed in the grid container
+    // build the game environment
+    buildGameEnvironment() {
+        // define the tetris environment
+        let tetrisEnvironment = document.querySelector('[data-tetrisContainer]');
+        // create the grid boxes inside the tetris environment
         let totalNumberOfGridBoxes = 200;
-        // count how many grid boxes have been built
-        let numberOfBoxesBuilt = 0;
+        // number of grid boxes built
+        let numberOfGridBoxesBuilt = 0;
+        // grid rows (for easily placing tetraminoes)
+        let gridRows = 0;
 
+        // build using a while loop
+        while (numberOfGridBoxesBuilt < totalNumberOfGridBoxes) {
+            //build a new grid box (as a div element)
+            let buildGridBox = document.createElement('div');
+            // set an attribute that makes it easily selectable by this javascript file and the value of the attribute to be the total number of grid boxes built
+            buildGridBox.setAttribute('data-gridBox', numberOfGridBoxesBuilt);
+            // append the built gridbox to the gameEnvironment
+            tetrisEnvironment.appendChild(buildGridBox);
 
+            // get the origin, where tetraminoes drop from
+            if (numberOfGridBoxesBuilt == 5) {
+                buildGridBox.setAttribute('data-tetraminoOrigin', 'true')
+            }
 
-        while (numberOfBoxesBuilt < totalNumberOfGridBoxes) {
-            // create box
-            let createBox = document.createElement('div');
-            let appendBox = tetrisCotainer.appendChild(createBox);
-            numberOfBoxesBuilt++;
-            console.log('yes');
+            // divide the environment into rows and columns
+            if (numberOfGridBoxesBuilt % 10 == 0) {
+                buildGridBox.setAttribute('data-gridRow', gridRows);
+                gridRows++;
+            }
+
+            // inncrement the number of gridboxes built by adding one
+            numberOfGridBoxesBuilt++;
         }
-        console.log(tetrisCotainer);
+
+        this.tetraminoPosition = tetraminoStartPosition;
+
     }
 
-    // startGame;
+    // -------basic game functionality
+
+    // start the game
     startGame() {}
 
-    // pause game
+    // pause the game
     pauseGame() {}
 
-    // stop / complete / end game
-    endGame() {}
+    // completeGame
+    completeGame() {}
 
-    // randomly select tetromino
-    selectTetromino() {}
-
-    // ----move tetromino
+    // ---------tetraminoes
+    selectTetramino() {}
 
     // rotate Tetramino
     rotateTetramino() {}
 
-    // move tetromino left
-    moveTetrominoLeft() {}
+    // move tetramino left
+    moveTetraminoLeft() {}
 
-    //  tetromino right
+    // move tetramino right
     moveTetraminoRight() {}
 
-    //move tetromino down
+    // move tetramino down by the second (call on interval)
     moveTetraminoDown() {}
 
-    // pull tetromino down
+    // pull tetramino down (after user has selected a shape and wants to save time)
     pullTetraminoDown() {}
-
 }
 
+// create an instanceof the tetris game
 const tetris = new Tetris();
+// build the game and start
+tetris.buildGameEnvironment();
 
 
-// add grid boxes to display tetraminoes inside the tetris container
-tetris.buildGame();
+// paint tetramino
+
+let T_Tetramino = tetraminoes[0][1];
+console.log(T_Tetramino);
