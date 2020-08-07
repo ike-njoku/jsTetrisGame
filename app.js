d8edca16ece096f09272fd22dd9068438878142c
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-08-06 17:43:40 -0700

    added function to determine positions of tetraminoes

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 // tetris game
-// important!!!!!!!!!!! this whole script is zeroBased... all countings start from 0 , not 1
+// important!!!!!!!! grid positions (gridBoxes) are 1 based(not zero based) for the purpose of calculations
 
 //------------- define tetraminoes
 // there are five tetraminoes and each have been rotated
 // therefore each tetramino has an array of possible rotations
 // so theres an two-dimensional array
 
+var startPoint = '';
 const tetraminoes = [
-    ['T-shape', [1, 2, 3, 5],
-        [2, 4, 5, 6],
-        [1, 4, 5, 7]
+    //T-Tetramino
+
+    [
+        // Tetramino cssClassName
+        'T-Tetramino',
+        // rotation one
+        [1, 2, 3, 12]
+
     ]
 ];
 
+
 // the start position for each tetramino
 const tetraminoStartPosition = [0, 5];
-
-// defint the maximum width for displaying tetraminoes, set it to three(confer drawTetramino)
-const tetraminoMaxWidth = 3;
 
 
 class Tetris {
@@ -26,13 +30,13 @@ class Tetris {
         // define the tetris environment
         let tetrisEnvironment = document.querySelector('[data-tetrisContainer]');
         // create the grid boxes inside the tetris environment
-        let totalNumberOfGridBoxes = 200;
+        let totalNumberOfGridBoxes = 221;
         // number of grid boxes built
-        let numberOfGridBoxesBuilt = 0;
+        let numberOfGridBoxesBuilt = 1;
         // grid rows (for easily placing tetraminoes)
-        let gridRows = 0;
+        let gridRows = 1;
         // grid columns (for easily placing tetraminoes)
-        let gridColumns = 0;
+        let gridColumns = 1;
 
         // build using a while loop
         while (numberOfGridBoxesBuilt < totalNumberOfGridBoxes) {
@@ -45,12 +49,12 @@ class Tetris {
 
             //---------- divide the environment into rows and columns
             // grid Rows
-            if (numberOfGridBoxesBuilt % 10 == 0) {
+            if (numberOfGridBoxesBuilt % 11 == 0) {
                 buildGridBox.setAttribute('data-gridRow', gridRows);
                 gridRows++;
             }
             //    grid Columns
-            buildGridBox.setAttribute('data-gridColumn', numberOfGridBoxesBuilt % 10);
+            buildGridBox.setAttribute('data-gridColumn', numberOfGridBoxesBuilt % 11);
 
             // inncrement the number of gridboxes built by adding one
             numberOfGridBoxesBuilt++;
@@ -105,10 +109,26 @@ const gridBoxes = document.querySelectorAll('[data-gridBox]');
 // paint tetramino
 
 let T_Tetramino = tetraminoes[0][1];
-console.log(T_Tetramino);
 
-function drawTetramino(startRow, startColumn) {
+function drawTetramino(tetraminoCssClassName, tetraminoShapeLayout, startRow, startColumn) {
+    //    define the value of the start point
+    startPoint = startColumn * startRow;
+    let newArray = [];
+
+    tetraminoShapeLayout.forEach((gridPosition) => {
+        gridPosition = startPoint + gridPosition;
+        newArray.push(gridPosition);
+    });
+    console.log(newArray);
+
+    gridBoxes.forEach((gridBox) => {
+        newArray.forEach((position) => {
+            if (gridBox.getAttribute('data-gridbox') == position) gridBox.classList.add(tetraminoCssClassName);
+        })
+
+    })
+
 
 }
 
-drawTetramino(0, 5);
+drawTetramino(tetraminoes[0][0], tetraminoes[0][1], 6, 10);
